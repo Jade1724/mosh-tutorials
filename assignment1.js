e2c674b76ccba25d14d926c7f398247667d0de5f
Haruka Ichinose <hic21@uclive.ac.nz>
2022-03-23 02:16:41 +1300

    Added assignment answer by Tim and my example solution.

--- assignment1.js
+++ assignment1.js
@@ -14,9 +14,14 @@ Consider which data type suits the best for each properties.
 You are not allowed to define all of them as mere strings. 
 */
 
-// let comouter = ... compelete it
-
-
+let computer = {
+    brand: 'Apple',
+    productName: 'MacBook Air',
+    price: 999.99,
+    hasSSD: false,
+    applications: ['VS Code', 'Goole Chrome'],
+    owner: null
+};
 
 /* Question 2 
 
@@ -28,10 +33,15 @@ You are not allowed to define all of them as mere strings.
 Finally, print out the updated object.
 */
 
+console.log(computer.brand);
 
+console.log(Math.round(computer.price));
 
+computer.applications.push('Postman');
 
+computer.owner = 'Haruka';
 
+console.log(computer);
 
 
 /* Question 3 
@@ -42,12 +52,11 @@ Question about the installed app.
 (c) Print out the position of Postman in the installed app.
 */
 
+console.log(computer.applications.length);
 
+console.log(computer.applications[1]);
 
-
-
-
-
+console.log(computer.applications.indexOf('Postman'));
 
 
 /* Question 4
@@ -59,6 +68,8 @@ The function should print out "Hello, I am {owner}'s computer."
 
 */
 
+computer.greeting = function () {
+    console.log("Hello, I am " + this.owner + "'s computer.");
+}
 
-
-// computer.greeting();  // This should printout the message. 
+computer.greeting();  // This should printout the message. 
